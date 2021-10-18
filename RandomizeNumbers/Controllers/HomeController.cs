3154702459174c953a4e3a2187dbe9e8e2cd09b4
Devon Blackbeard <32315720+devonblackbeard@users.noreply.github.com>
2021-10-17 22:11:28 -0600

    remove bullet list style

--- RandomizeNumbers/Controllers/HomeController.cs
+++ RandomizeNumbers/Controllers/HomeController.cs
@@ -22,16 +22,37 @@ namespace RandomizeNumbers.Controllers
         {
             return View();
         }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
+      
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        [HttpPost]
+        public IActionResult GenerateNumbers()
+        {
+            Random rand = new Random();
+            List<int> listOfInt = Enumerable.Range(1, 10000).ToList();
+
+            int count = listOfInt.Count;
+
+            for (int i = 0; i < count; i++)
+            {
+                int index = i + rand.Next(count - i);
+                int temp = listOfInt[index];
+                //perform swap
+                listOfInt[index] = listOfInt[i];
+                listOfInt[i] = temp;
+            }
+
+            if (listOfInt.Count != listOfInt.Distinct().Count())
+            {
+                throw new Exception();
+            }
+
+            return View("Index", listOfInt);
+        }
+
     }
 }
